996417aec6f0f1301326d48fa405809dbf6c6015
johnniang <johnniang@fastmail.com>
2022-03-15 17:13:06 +0800

    Init maven sample project
    Signed-off-by: johnniang <johnniang@fastmail.com>

---
+++ src/main/java/io/kubesphere/devops/sample/App.java
@@ -0,0 +1,38 @@
+package io.kubesphere.devops.sample;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 6666), 0);
+        server.setExecutor(Executors.newFixedThreadPool(10));
+        server.createContext("/greeting", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String greeting = "Hello, KubeSphere DevOps from maven sample!\n";
+                OutputStream response = exchange.getResponseBody();
+                exchange.sendResponseHeaders(200, greeting.length());
+                response.write(greeting.getBytes(StandardCharsets.UTF_8));
+                response.flush();
+                response.close();
+            }
+        });
+        LOGGER.info("Server started listening " + server.getAddress().getHostName() + ":" + server.getAddress().getPort());
+        server.start();
+    }
+}
